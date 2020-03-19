7b3620cdc5cb8ec573135a9d05ce527274a73d0b
Boehm Hugo <hugo.boehm98@gmail.com>
2020-03-19 15:46:19 +0100

    ajout vue main_parent

---
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_gestion_des_taches.java
@@ -0,0 +1,14 @@
+package fr.ynov.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_gestion_des_taches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_des_taches);
+    }
+}
---
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_main_parent.java
@@ -0,0 +1,35 @@
+package fr.ynov.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_main_parent extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_parent);
+        Button button_ajouter_une_tache = findViewById(R.id.button_ajouter_une_tache);
+        Button button_ajouter_un_reveil =  findViewById(R.id.button_ajouter_un_reveil);
+        button_ajouter_une_tache.setOnClickListener(this);
+        button_ajouter_un_reveil.setOnClickListener(this);
+        Log.d("test", "build: build");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_ajouter_une_tache) {
+            Intent gestion_des_taches_view = new Intent(getApplicationContext(), Activity_gestion_des_taches.class);
+            startActivity(gestion_des_taches_view);
+        }
+        if(v.getId() == R.id.button_ajouter_un_reveil) {
+
+        }
+
+    }
+}
