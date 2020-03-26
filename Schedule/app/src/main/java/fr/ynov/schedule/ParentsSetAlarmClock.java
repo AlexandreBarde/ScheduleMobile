5ce131a504752c1a76dda11987a99398ed68e8a6
Fantemis <adel.mekki1998@hotmail.com>
2020-03-26 12:02:46 +0100

    Set alarm buttons front

--- Schedule/app/src/main/java/fr/ynov/schedule/ParentsSetAlarmClock.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/ParentsSetAlarmClock.java
@@ -4,19 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParentsSetAlarmClock extends AppCompatActivity implements View.OnClickListener {
+    Map clickedButtons = new HashMap<String, Boolean>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parents_set_alarm_clock);
+
+        clickedButtons.put("Lundi", false);
+        clickedButtons.put("Mardi", false);
+        clickedButtons.put("Mercredi", false);
+        clickedButtons.put("Jeudi", false);
+        clickedButtons.put("Vendredi", false);
 
         Button setAlarm = findViewById(R.id.setAlarm);
         setAlarm.setOnClickListener(this);
@@ -31,16 +45,49 @@ public class ParentsSetAlarmClock extends AppCompatActivity implements View.OnCl
         {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.SET_ALARM}, 0);
-
-
+            
             TimePicker picker=(TimePicker)findViewById(R.id.alarmTimePicker);
             picker.setIs24HourView(true);
-
-            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-            intent.putExtra(AlarmClock.EXTRA_HOUR,picker.getHour());
-            intent.putExtra(AlarmClock.EXTRA_MINUTES,picker.getMinute());
-
-            startActivity(intent);
+        }
+        if(view.getId() == R.id.lundi) {
+            clickedButtons.put("Lundi", !(Boolean)clickedButtons.get("Lundi"));
+            if((Boolean)clickedButtons.get("Lundi") == true) {
+                view.setBackgroundColor(Color.parseColor("#03DAC5"));
+            } else {
+                view.setBackgroundColor(Color.parseColor("#FFCACACA"));
+            }
+        }
+        if(view.getId() == R.id.mardi) {
+            clickedButtons.put("Mardi", !(Boolean)clickedButtons.get("Mardi"));
+            if((Boolean)clickedButtons.get("Mardi") == true) {
+                view.setBackgroundColor(Color.parseColor("#03DAC5"));
+            } else {
+                view.setBackgroundColor(Color.parseColor("#FFCACACA"));
+            }
+        }
+        if(view.getId() == R.id.mercredi) {
+            clickedButtons.put("Mercredi", !(Boolean)clickedButtons.get("Mercredi"));
+            if((Boolean)clickedButtons.get("Mercredi") == true) {
+                view.setBackgroundColor(Color.parseColor("#03DAC5"));
+            } else {
+                view.setBackgroundColor(Color.parseColor("#FFCACACA"));
+            }
+        }
+        if(view.getId() == R.id.jeudi) {
+            clickedButtons.put("Jeudi", !(Boolean)clickedButtons.get("Jeudi"));
+            if((Boolean)clickedButtons.get("Jeudi") == true) {
+                view.setBackgroundColor(Color.parseColor("#03DAC5"));
+            } else {
+                view.setBackgroundColor(Color.parseColor("#FFCACACA"));
+            }
+        }
+        if(view.getId() == R.id.vendredi) {
+            clickedButtons.put("Vendredi", !(Boolean)clickedButtons.get("Vendredi"));
+            if((Boolean)clickedButtons.get("Vendredi") == true) {
+                view.setBackgroundColor(Color.parseColor("#03DAC5"));
+            } else {
+                view.setBackgroundColor(Color.parseColor("#FFCACACA"));
+            }
         }
     }
 }
