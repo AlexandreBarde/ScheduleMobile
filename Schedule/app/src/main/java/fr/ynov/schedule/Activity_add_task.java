57e49b959fa23371bb9af257a7af6b6c6f3ab943
Hugo Boehm <hugo.boehm98@gmail.com>
2020-03-26 17:38:04 +0100

    add task backend V1

--- Schedule/app/src/main/java/fr/ynov/schedule/Activity_add_task.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_add_task.java
@@ -3,9 +3,17 @@ package fr.ynov.schedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
-public class Activity_add_task extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Activity_add_task extends AppCompatActivity implements OnSuccessListener<DocumentReference>, View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,27 @@ public class Activity_add_task extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
         TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
         picker.setIs24HourView(true);
+        Button addtask = findViewById(R.id.button_ajouter_la_tache);
+        addtask.setOnClickListener(this);
+    }
 
+    @Override
+    public void onSuccess(DocumentReference documentReference) {
+        Log.d("xxxx","Insertion Ok");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_ajouter_la_tache) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            EditText edit_name = findViewById(R.id.name_new_task);
+            String  new_name_text = edit_name.getText().toString();
+            EditText edit_description = findViewById(R.id.description_new_task);
+            String  new_description_text = edit_description.getText().toString();
+            Task new_task = new Task(new_name_text, new_description_text, "9H00", 1);
+            db.collection("Task").add(new_task).addOnSuccessListener(this);
+
+
+        }
     }
 }
--- Schedule/app/src/main/java/fr/ynov/schedule/Activity_gestion_des_taches.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_gestion_des_taches.java
@@ -42,6 +42,7 @@ public class Activity_gestion_des_taches extends AppCompatActivity implements Vi
 
 
 
+
     }
 
     @Override
@@ -61,7 +62,7 @@ public class Activity_gestion_des_taches extends AppCompatActivity implements Vi
         for(DocumentSnapshot doc : documents) {
             Map<String, Object> map = doc.getData();
             int image_task;
-            switch (doc.get("Status").toString()) {
+            switch (doc.get("image_status").toString()) {
                 case "1" :
                     Log.i("xxxx", "image task : ok");
                     image_task = R.drawable.image_task_green;
@@ -77,7 +78,7 @@ public class Activity_gestion_des_taches extends AppCompatActivity implements Vi
 
 
             }
-            list_task.add(new Task(doc.get("Nom").toString(), doc.get("Description").toString(), "9H00", image_task ));
+            list_task.add(new Task(doc.get("name").toString(), doc.get("description").toString(), "9H00", image_task ));
         }
 
         recyclerView = findViewById(R.id.taches_list);
