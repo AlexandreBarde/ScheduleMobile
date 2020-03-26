c73be9a2aa6eb5735186d582d4ad53b925009b67
Hugo Boehm <hugo.boehm98@gmail.com>
2020-03-26 16:21:11 +0100

    Vue add task V1 scrollable

--- Schedule/app/src/main/java/fr/ynov/schedule/Activity_add_task.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_add_task.java
@@ -3,6 +3,7 @@ package fr.ynov.schedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class Activity_add_task extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Activity_add_task extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+        picker.setIs24HourView(true);
+
     }
 }
--- Schedule/app/src/main/java/fr/ynov/schedule/Activity_gestion_des_taches.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_gestion_des_taches.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TimePicker;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.firestore.DocumentReference;
