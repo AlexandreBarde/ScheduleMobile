1a3c3584a9ad45631226a7500da39f045ca47cf5
Hugo Boehm <hugo.boehm98@gmail.com>
2020-04-09 16:01:44 +0200

    Add a propos view

---
+++ Schedule/app/src/main/java/fr/ynov/schedule/Activity_a_propos.java
@@ -0,0 +1,14 @@
+package fr.ynov.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_a_propos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_propos);
+    }
+}
