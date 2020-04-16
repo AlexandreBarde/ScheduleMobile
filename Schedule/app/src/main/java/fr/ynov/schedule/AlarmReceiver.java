622580a9e0cea18fb393508f9d8023c00e35e0de
Fantemis <adel.mekki1998@hotmail.com>
2020-04-16 17:27:30 +0200

    redirection sur la bonne activité lors de la sonnerie du réveil

--- Schedule/app/src/main/java/fr/ynov/schedule/AlarmReceiver.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/AlarmReceiver.java
@@ -95,21 +95,4 @@ public class AlarmReceiver extends BroadcastReceiver {
         notificationManager.notify(1,notification);
         WakeLocker.release();
     }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = c_context.getString(R.string.channel_name);
-            String description = c_context.getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "nouveau_channel_id", NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = c_context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
 }
