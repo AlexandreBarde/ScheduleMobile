88219642cf205601357802bd9a41626a0c54d102
Hugo Boehm <hugo.boehm98@gmail.com>
2020-04-30 11:07:42 +0200

    New layout notify task

--- Schedule/app/src/main/java/fr/ynov/schedule/TaskNotificationLayout.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/TaskNotificationLayout.java
@@ -3,12 +3,24 @@ package fr.ynov.schedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaskNotificationLayout extends AppCompatActivity {
+
+    Task task_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_notification_layout);
+        task_test = new Task("Test","Description test",1588265280231L, "todo", 1, 5L);
+        TextView name_task = findViewById(R.id.name_task);
+        name_task.setText(task_test.getName());
+        TextView description_task = findViewById(R.id.description_task);
+        description_task.setText(task_test.getDescription());
+        TextView heure_tache = findViewById(R.id.heure_tache);
+        heure_tache.setText("Date de la tache : " + task_test.dateFormat());
+        TextView duree_tache = findViewById(R.id.durée_tache);
+        duree_tache.setText("Durée de la tache : " + task_test.heureFormat());
     }
 }
