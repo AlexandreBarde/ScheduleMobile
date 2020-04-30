ee556f6de971b0292722a8c0eaea1d4d3ea304d6
Alexandre <alexandre.barde@thalesaleniaspace.com>
2020-04-30 11:52:04 +0200

    Changement de dossier + suppression des Logs

--- Schedule/app/src/main/java/fr/ynov/schedule/MainChildActivity.java
+++ Schedule/app/src/main/java/fr/ynov/schedule/MainChildActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import fr.ynov.schedule.calendar.CalendarActivity;
+
 public class MainChildActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
